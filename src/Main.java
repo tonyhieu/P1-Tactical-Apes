b6e81f79ce4c440d59e9bed6dfb0ae695f1b0e03
Eyaad-Emaad <eyaadmir@gmail.com>
2020-12-21 00:57:37 -0800

    Created menu for simulations

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,52 @@
+import DeathGame.*;
+import DoublePendulum.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
 
 public class Main {
-
     public static void main (String[] args) {
+        //Initialize menu objects
+        JFrame menuFrame = new JFrame();
+        JPanel menuPanel = new JPanel();
+        JLabel welcomeLabel = new JLabel("Tactical Apes Simulations");
+        JButton deathGame = new JButton("Death Game Simulation");
+        JButton doublePendulum = new JButton("Double Pendulum Simulation");
 
+        //frame settings
+        menuFrame.setSize(500, 500);
+        menuFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        menuFrame.setVisible(true);
+        menuFrame.add(menuPanel);
+
+        //panel add objects
+        menuPanel.setLayout(null);
+        menuPanel.add(welcomeLabel);
+        menuPanel.add(deathGame);
+        menuPanel.add(doublePendulum);
+
+        //setting bounds
+        welcomeLabel.setBounds(150,50,200,50);
+        deathGame.setBounds(100,150,250,100);
+        doublePendulum.setBounds(100,300,250,100);
+
+        //button logic
+        deathGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DeathGameView deathgameview = new DeathGameView();
+                deathgameview.RunDeathGameView();
+            }
+        });
+        doublePendulum.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DoublePendulumView doublependulumview = new DoublePendulumView();
+                doublependulumview.startSimulation();
+            }
+        });
 
 
     }
