c833ae341c328e7151e2ef1e4e5efe666dc42e86
Hiếu Võ <54915685+tonyhieu@users.noreply.github.com>
2021-01-11 08:09:07 -0800

    remove participant class

--- src/DeathGame/DeathGameView.java
+++ src/DeathGame/DeathGameView.java
@@ -265,32 +265,3 @@ public class DeathGameView {
     }
 
 }
-
-//temp fix while i try to figure out why these files arent working
-class Participant {
-
-    //values for various relationships
-    public final int STRANGER = 0;
-    public final int FRIEND = 1;
-    public final int CLOSEFRIENDS = 2;
-
-    public int intel;
-    public int str;
-    public int soc;
-    public int emp;
-    public Hashtable<Participant, Integer> relationships = new Hashtable<Participant, Integer>();
-    public String name;
-
-    public float deathChance = 0;
-    public float killChance = 0;
-
-    public Participant (int intel, int str, int soc, int emp, Hashtable<Participant, Integer> relationships, String name) {
-        this.intel = intel;
-        this.str = str;
-        this.soc = soc;
-        this.emp = emp;
-        this.relationships = relationships;
-        this.name = name;
-    }
-
-}
